544c4d888cc844d97d42f9af473733a037ee8691
Carene760 <carenerazafy3@gmail.com>
2026-01-26 19:03:24 +0300

    update mampandeha
    Je crois

--- signalement_route/backend_local/src/test/java/com/example/gestion/GestionApplicationTests.java
+++ signalement_route/backend_local/src/test/java/com/example/gestion/GestionApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.gestion;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.back.GestionApplication;
+
+@SpringBootTest(classes = GestionApplication.class)
 class GestionApplicationTests {
 
 	@Test
