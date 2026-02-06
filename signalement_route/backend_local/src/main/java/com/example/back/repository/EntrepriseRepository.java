4dd5f6d777d6266cec0f367ac8bff2d2a1ae5de0
Carene760 <carenerazafy3@gmail.com>
2026-02-06 13:31:27 +0300

    mandamina
    Ionic Vue JS
    Application mobile

--- signalement_route/backend_local/src/main/java/com/example/back/repository/EntrepriseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.back.repository;
-
-import com.example.back.models.Entreprise;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EntrepriseRepository extends JpaRepository<Entreprise, Long> {
-}
